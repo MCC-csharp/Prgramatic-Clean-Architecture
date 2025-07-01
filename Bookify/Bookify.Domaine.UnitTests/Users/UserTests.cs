d89750d51b183c9e830fa42b9d46a64a6b6af717
mcc-csharp <couchotm@gmail.com>
2025-07-01 18:49:07 +0200

    Update test project for .NET 8.0 and add user tests
    - Target .NET 8.0 in `Bookify.Domaine.UnitTests.csproj` with implicit usings and nullable reference types.
    - Introduce `UserData` class for predefined user data.
    - Create `UserTests` class with a method to verify user property values.
    - Add necessary binary files for dependencies like `FluentAssertions`, `MediatR.Contracts`, and `Microsoft.TestPlatform`.
    - Generate `deps.json` and `runtimeconfig.json` for updated project configuration.
    - Update and create configuration files to reflect new package dependencies.
    - Revise assembly information and global usings for consistency.
    - Include resource files for localization and package metadata.

---
+++ Bookify/Bookify.Domaine.UnitTests/Users/UserTests.cs
@@ -0,0 +1,25 @@
+﻿using Bookify.Domain.Users;
+using FluentAssertions;
+using Xunit;
+
+
+namespace Bookify.Domaine.UnitTests.Users
+{
+    public class UserTests
+    {
+        [Fact]
+        public void Create_Should_SetPropertyValues()
+        {
+            // Act
+            var user = User.Create(UserData.FirstName,
+                UserData.LastName,
+                UserData.Email
+            );
+
+            // Assert
+            user.FirstName.Should().Be(UserData.FirstName);
+            user.LastName.Should().Be(UserData.LastName);
+            user.Email.Should().Be(UserData.Email);
+        }
+    }
+}
