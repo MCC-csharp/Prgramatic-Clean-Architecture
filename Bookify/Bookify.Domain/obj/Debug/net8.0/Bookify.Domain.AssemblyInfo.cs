4bdbe858aa65a46a1d1d142a43bb8f5ad304fb89
mcc-csharp <couchotm@gmail.com>
2025-07-01 17:13:35 +0200

    Update Bookify.Domain for .NET 8.0 and MediatR 2.0.1
    - Updated `Bookify.Domain.deps.json` to target .NET Core 8.0 and added `MediatR.Contracts` dependency version 2.0.1.
    - Added binary files: `Bookify.Domain.dll` and `Bookify.Domain.pdb`.
    - Updated `AssemblyInformationalVersion` in `Bookify.Domain.AssemblyInfo.cs`.
    - Modified `Bookify.Domain.AssemblyInfoInputs.cache` and `Bookify.Domain.csproj.CoreCompileInputs.cache` with new hash values.
    - Updated `Bookify.Domain.csproj.FileListAbsolute.txt` with paths to new binaries.
    - Added `Bookify.Domain.sourcelink.json` for source linking support.

--- Bookify/Bookify.Domain/obj/Debug/net8.0/Bookify.Domain.AssemblyInfo.cs
+++ Bookify/Bookify.Domain/obj/Debug/net8.0/Bookify.Domain.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6a41b329754441d7ede523b0effffff134b3fc51")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cb26393bbdd84d1947ee510b24043af96f4b41c0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Bookify.Domain")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
